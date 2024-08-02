969eb582e131762d71e8ebcbc4656a1e75badf43
Dark Coder <codewithdark90@gmail.com>
2024-08-02 18:21:20 +0500

    Update session_state.py

--- utils/session_state.py
+++ utils/session_state.py
@@ -3,6 +3,8 @@ import streamlit as st
 def initialize_session_state():
     if 'user' not in st.session_state:
         st.session_state.user = None
+    if 'password' not in st.session_state:
+        st.session_state.password = None
     if 'current_question_index' not in st.session_state:
         st.session_state.current_question_index = 0
     if 'job_position' not in st.session_state:
@@ -11,3 +13,5 @@ def initialize_session_state():
         st.session_state.interview_completed = False
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
+    if 'users' not in st.session_state:
+        st.session_state.users = {}
