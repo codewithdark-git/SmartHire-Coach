f7a1c179a16028bba98caae9b0d7151e74bf86bd
Dark Coder <codewithdark90@gmail.com>
2024-08-05 11:45:56 +0500

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,34 +1,36 @@
+from typing import List
+import streamlit as st
 import PyPDF2
-import docx
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-nltk.download('punkt')
-nltk.download('stopwords')
+# import docx
 
 
+@st.cache_data
 
-def extract_text_from_pdf(file):
-    pdf_reader = PyPDF2.PdfFileReader(file)
+def read_pdf(file):
+    pdf_reader = PyPDF2.PdfReader(file)
     text = ""
-    for page_num in range(pdf_reader.getNumPages()):
-        text += pdf_reader.getPage(page_num).extract_text()
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
     return text
 
-def extract_text_from_docx(file):
-    doc = docx.Document(file)
-    text = ""
-    for para in doc.paragraphs:
-        text += para.text
-    return text
+def read_file(file):
+    if file.type == "application/pdf":
+        return read_pdf(file)
+    # elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+    #     return read_docx(file)
+    elif file.type == "text/plain":
+        return file.read().decode("utf-8")
+    else:
+        return "Unsupported file type"
 
-def preprocess_text(text):
-    text = text.lower()
-    tokens = word_tokenize(text)
-    tokens = [t for t in tokens if t.isalpha()]
-    stop_words = set(stopwords.words('english'))
-    tokens = [t for t in tokens if t not in stop_words]
-    return ' '.join(tokens)
 
+# def read_docx(file):
+#     doc = docx.Document(file)
+#     return "\n".join([para.text for para in doc.paragraphs])
+
+
+def split_text_into_chunks(text, chunk_size=3000):
+    words = text.split()
+    for i in range(0, len(words), chunk_size):
+        yield ' '.join(words[i:i + chunk_size])
